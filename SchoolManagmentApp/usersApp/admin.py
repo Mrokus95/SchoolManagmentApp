ad18185818ffebc682b23497113fe50276950b55
Lukasz Mroczkowski <luk.mroczkowski@gmail.com>
2023-07-22 19:16:54 +0200

    userApp - Fixed logic mistakes in models

--- SchoolManagmentApp/usersApp/admin.py
+++ SchoolManagmentApp/usersApp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Profile, Parent, ClassUnit, Student
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'photo', 'phone_number', 'account_type']
+    list_filter = ['user', 'account_type', 'phone_number']
+    search_fields = ['user', 'phone_number']
+    ordering = ['account_type', 'user']
+
+@admin.register(Parent)
+class ParentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ClassUnit)
+class ClassUnitAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    pass
--- SchoolManagmentApp/usersApp/models.py
+++ SchoolManagmentApp/usersApp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
-
+from datetime import datetime
 # Users
 
 class Profile(models.Model):
@@ -21,30 +21,39 @@ class Profile(models.Model):
     photo = models.ImageField(upload_to='users/avatars/', default='users/avatars/anonymous.png', blank=True)
     phone_number = models.CharField(max_length=9, blank=True, null=True)
     account_type = models.CharField(max_length=10, choices=TYPE_ACCOUNT_CHOICES, default=STUDENT)
-    last_name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return f'{self.user.first_name} {self.last_name}'
-    
-class Parent(models.Model):
-    user = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name='parent')
-    
+        return f'{self.user.first_name} {self.user.last_name} \'s profile'
+        
+
 class ClassUnit(models.Model):
-    start_year = models.DateField()
-    study_year = models.IntegerField(validators=[
-    MinValueValidator(1),
-    MaxValueValidator(7)
-    ])
-    letter_mark = models.CharField(max_length=1, choices=[('A', 'A'), ('B', 'B'), ('C', 'C'), ('D', 'D'), ('E', 'E'), ('F', 'F')], null=False, blank=False)
+    start_year = models.IntegerField(
+        validators=[MinValueValidator(2023), ], default=2023
+    )
+    study_year = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(7)],
+        default=1
+    )
+    letter_mark = models.CharField(
+        max_length=1,
+        choices=[('A', 'A'), ('B', 'B'), ('C', 'C'), ('D', 'D'), ('E', 'E'), ('F', 'F')],
+        null=False,
+        blank=False
+    )
 
     def __str__(self):
-        return f"{self.start_year} {self.letter_mark}"
+        return f"Class {self.study_year}{self.letter_mark}"
 
 class Student(models.Model):
-    user = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name='student')
-    parent = models.OneToOneField(Parent, on_delete=models.CASCADE)
+    user = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name='student', limit_choices_to={'account_type': 'Student'})
     klasa = models.OneToOneField(ClassUnit, models.DO_NOTHING, blank=False, null=False, related_name='students_in_class')
 
     def __str__(self):
-        return f'{self.user.user.first_name} {self.user.user.last_name}'
+        return f'{self.user.user.first_name} {self.user.user.last_name} - student'
 
+class Parent(models.Model):
+    user = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name='parent', limit_choices_to={'account_type': 'Parent'})
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='children')
+
+class Teacher(models.Model):
+    user = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name='teacher')

36a607b9e008b797946674c0a67005f41a721cf7
Mat <mmmateuszmichalski@gmail.com>
2023-07-23 09:55:36 +0200

    events app start

--- SchoolManagmentApp/SchoolManagmentApp/urls.py
+++ SchoolManagmentApp/SchoolManagmentApp/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('usersApp.urls')),
+    path('', include('eventApp.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

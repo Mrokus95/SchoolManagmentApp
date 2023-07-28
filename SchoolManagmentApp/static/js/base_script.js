c1d328e5b8a5ccc8cd44ebeb55d4dc4fc06778bc
Mat <mmmateuszmichalski@gmail.com>
2023-07-28 13:52:02 +0200

    almost donte foncfirm window

--- SchoolManagmentApp/static/js/base_script.js
+++ SchoolManagmentApp/static/js/base_script.js
@@ -1,17 +1,47 @@
-// Pobieramy elementy z DOM
+//  variables
 const filterButton = document.getElementById("filterButton");
 const filterWindow = document.getElementById("filterWindow");
 const filterButtonClose = document.getElementById("filterButtonClose");
+const myDeleteConfirmer = document.getElementById("myDeleteConfirmer");
+const confirmButton = document.getElementById("deleteConfirmButton");
+const cancelButton = document.getElementById("deleteCancelButton");
+const deleteButton = document.getElementById("deleteEvent");
 
 
 
-// Dodajemy obsługę kliknięcia guzika
-filterButton.addEventListener("click", function() {
-    filterWindow.style.display = "block";
+// deleter siplay
+myDeleteConfirmer.classList.add("hidden");
+
+deleteButton.addEventListener("click", function (event) {
+  event.preventDefault();
 });
 
-// Dodajemy obsługę kliknięcia przycisku zamykania
-filterButtonClose.addEventListener("click", function() {
-    filterWindow.style.display = "none";
+confirmButton.addEventListener('click', function() {
+    hideDialog();
+    proceedToDelete();
 });
 
+// schowanie okna potwierdzenia w przypadku kliknięcia anuluj
+cancelButton.addEventListener("click", function () {
+  hideDialog();
+});
+
+// funkcja do wyjebania zasobu
+function proceedToDelete() {
+  const deleteLink = document.getElementById("deleteEvent");
+  const url = deleteLink.getAttribute("href");  // Pobranie adresu URL z atrybutu "href"
+  window.location.href = url
+}
+
+function showDialog() {
+  myDeleteConfirmer.classList.remove("hidden"); 
+  myDeleteConfirmer.classList.add("animate__slideInDown");  
+}
+
+function hideDialog() {
+  myDeleteConfirmer.classList.add("animate__slideOutUp");
+  myDeleteConfirmer.addEventListener("animationend", function () {
+    myDeleteConfirmer.classList.add("hidden");
+    myDeleteConfirmer.classList.remove("animate__slideOutUp");
+  });
+}

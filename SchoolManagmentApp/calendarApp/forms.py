4fed99ba0384cffe6ebdb4a81c17fe00a344059a
Lukasz Mroczkowski <luk.mroczkowski@gmail.com>
2023-08-03 14:50:24 +0200

    Added veryfication for teacher-subject pair in forms

--- SchoolManagmentApp/calendarApp/forms.py
+++ SchoolManagmentApp/calendarApp/forms.py
@@ -1,11 +1,25 @@
 from django import forms
+from django.shortcuts import get_object_or_404
 from .models import Lesson
+from eventApp.models import Teacher
 from usersApp.models import ClassUnit
 
 class LessonForm(forms.ModelForm):
     class Meta:
         model = Lesson
         fields = ['subject', 'teacher', 'classroom', 'is_base']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        subject = cleaned_data.get('subject')
+        teacher = cleaned_data.get('teacher')
+
+        if subject not in teacher.lesson_type.all():
+
+            raise forms.ValidationError("Selected subject is not taught by the selected teacher.")
+
+        return cleaned_data
+    
 
 
 class EditLessonForm(forms.ModelForm):
@@ -18,12 +32,19 @@ class EditLessonForm(forms.ModelForm):
         cleaned_data = super().clean()
         is_base = cleaned_data.get('is_base')
         is_cancelled = cleaned_data.get('is_cancelled')
+        subject = cleaned_data.get('subject')
+        teacher = cleaned_data.get('teacher')
 
         if is_base and is_cancelled:
-            raise forms.ValidationError("Both 'is_base' and 'is_cancelled' cannot be selected at the same time.")
+            raise forms.ValidationError("Both 'is base' and 'is cancelled' cannot be selected at the same time.")
         
-        return cleaned_data
+        if subject not in teacher.lesson_type.all():
 
+            raise forms.ValidationError("Selected subject is not taught by the selected teacher.")
+
+        return cleaned_data
+    
+    
 
 class ClassUnitForm(forms.ModelForm):
     class_unit = forms.ModelChoiceField(queryset=ClassUnit.objects.all(), label='Class:')

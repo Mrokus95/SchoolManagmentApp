09cc12b203ddf1541931c4ca60c31f9edd1f25c1
Mat <mmmateuszmichalski@gmail.com>
2023-08-08 17:12:31 +0200

    minor js location changes

--- SchoolManagmentApp/static/js/base_script.js
+++ SchoolManagmentApp/static/js/base_script.js
@@ -1,17 +1,1 @@
 
-
-// filtering
-
-const filterButton = document.getElementById("filterButton");
-const filterWindow = document.getElementById("filterWindow");
-const filterButtonClose = document.getElementById("filterButtonClose");
-
-
-filterButton.addEventListener("click", function() {
-  filterWindow.style.display = "block";
-});
-
-
-filterButtonClose.addEventListener("click", function() {
-  filterWindow.style.display = "none";
-});
